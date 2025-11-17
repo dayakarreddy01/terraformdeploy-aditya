6e81633ee3722c0e846bbd2b2453f03b6ef047a9
Adityah669 <adithyasantosht@gmail.com>
2025-11-17 19:44:55 +0530

    Delete Folder directory

--- Folder/backend.tf
+++
@@ -1,9 +0,0 @@
-
-terraform {
-  backend "azurerm" {}
-  required_providers {
-    azurerm = { source = "hashicorp/azurerm", version = "~> 3.111" }
-    azapi  = { source = "azure/azapi",       version = "~> 1.13"  }
-  }
-}
-provider "azurerm" { features {} }
