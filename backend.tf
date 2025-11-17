254cc715baf6c607f6ed52cc607139fb22d65dec
Adityah669 <adithyasantosht@gmail.com>
2025-11-17 19:44:39 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {}
+  required_providers {
+    azurerm = { source = "hashicorp/azurerm", version = "~> 3.111" }
+    azapi  = { source = "azure/azapi",       version = "~> 1.13"  }
+  }
+}
+provider "azurerm" { features {} }
