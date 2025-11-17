c5860bf4958a43b289375b26d74592954dcea7fc
Adityah669 <adithyasantosht@gmail.com>
2025-11-17 19:56:01 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,6 @@ terraform {
   backend "azurerm" {}
   required_providers {
     azurerm = { source = "hashicorp/azurerm", version = "~> 3.111" }
-    azapi  = { source = "azure/azapi",       version = "~> 1.13"  }
   }
 }
 provider "azurerm" { features {} }
