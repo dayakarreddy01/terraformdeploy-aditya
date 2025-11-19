b987eeb7ad9dfe762071d2e27c407000112a9817
Adityah669 <adithyasantosht@gmail.com>
2025-11-19 19:46:29 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "rg_name"                { type = string }
+variable "location"               { type = string }
+variable "logic_app_name"         { type = string }
+variable "storage_account_name"   { type = string }
+variable "storage_container_name" { type = string }
+
+variable "blob_connection_name"        { type = string }
+variable "blob_connection_display_name"{ type = string }
