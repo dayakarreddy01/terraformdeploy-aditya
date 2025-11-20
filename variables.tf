7abc50e888afe9616c2a467cbeef095f0784c952
Adityah669 <adithyasantosht@gmail.com>
2025-11-20 18:40:10 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,40 @@
-variable "rg_name"                { type = string }
-variable "location"               { type = string }
-variable "logic_app_name"         { type = string }
-variable "storage_account_name"   { type = string }
-variable "storage_container_name" { type = string }
+variable "rg_name" {
+  type    = string
+  default = "New_LA"
+}
 
-variable "blob_connection_name"        { type = string }
-variable "blob_connection_display_name"{ type = string }
+variable "location" {
+  type    = string
+  default = "East US"
+}
+
+variable "logic_app_name" {
+  type    = string
+  default = "New_LA" # Legal for Logic Apps Consumption
+}
+
+variable "storage_account_name" {
+  type    = string
+  default = "mynewstoragestr123" # must be lowercase, 3–24 chars, globally unique
+}
+
+variable "storage_container_name" {
+  type    = string
+  default = "demonewconnect"
+}
+
+variable "blob_connection_name" {
+  type    = string
+  default = "Newconnection"
+}
+
+variable "blob_connection_display_name" {
+  type    = string
+  default = "newconnection"
+}
+
+ //You don't need subscription_id if using data.azurerm_client_config.current.
+variable "subscription_id" {
+  type    = string
+  default = "b26b0c24-15bd-4527-bf7b-970aab26312b"
+}
