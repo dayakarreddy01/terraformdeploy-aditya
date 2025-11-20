0cbcd7db9d9d55decfe073713970eaa349053afc
Adityah669 <adithyasantosht@gmail.com>
2025-11-20 18:55:31 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,17 +1,10 @@
 terraform {
   required_version = ">= 1.4.0"
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.100"
-    }
-    azapi = {
-      source  = "azure/azapi"
-      version = "~> 1.12"
-    }
-  }
-}
+ required_providers {
+  azurerm = { source = "hashicorp/azurerm", version = "~> 3.100" }
+  azapi   = { source = "azure/azapi",    version = "~> 1.12" } # or "~> 2.0" if tested
+
 
 provider "azurerm" {
   features {}
